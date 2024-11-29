fde99116e5dc11c82060759a813653e79c2f295c
Wenlong Zhao <wenlong.zhao.97@gmail.com>
2024-11-29 15:28:00 -0500

    refactor config

---
+++ exp_launch/010_eval_new_score_type/eval_T0HO_phatgoose_original.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+hostname
+source exp_launch/setup_environment_variable.sh
+
+python src/launch_single_process.py \
+\
+--gin_files \
+colm/datasets/p3_t5xl.gin \
+colm/datasets/flanv2_t5xl.gin \
+colm/datasets/bigbench.gin \
+exp_launch/010_eval_new_score_type/eval_T0HO_phatgoose_paper.gin
